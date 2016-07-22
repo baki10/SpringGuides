934a2c3e5274610300a9a305a933d483d00096fb
Ilmir Bakirov <ilmirbakirov@gmail.com>
2016-07-23 00:19:57 +0500

    form submission

---
+++ Form-submission/src/main/java/com/bakigoal/Greeting.java
@@ -0,0 +1,26 @@
+package com.bakigoal;
+
+/**
+ * Created by ilmir on 21.07.16.
+ */
+public class Greeting {
+
+  private long id;
+  private String content;
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public String getContent() {
+    return content;
+  }
+}
