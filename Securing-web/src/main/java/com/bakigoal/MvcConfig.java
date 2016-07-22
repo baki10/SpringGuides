0323c1fbb9767e76ca21ce7599597d8e7a9ab653
Ilmir Bakirov <ilmirbakirov@gmail.com>
2016-07-22 17:21:36 +0500

    web security

---
+++ Securing-web/src/main/java/com/bakigoal/MvcConfig.java
@@ -0,0 +1,20 @@
+package com.bakigoal;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by ilmir on 22.07.16.
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/home").setViewName("home");
+    registry.addViewController("/").setViewName("home");
+    registry.addViewController("/hello").setViewName("hello");
+    registry.addViewController("/login").setViewName("login");
+  }
+}
