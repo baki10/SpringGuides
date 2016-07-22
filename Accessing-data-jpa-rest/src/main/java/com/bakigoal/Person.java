a0ad828e287667ed7df5a7e54f3524113c978543
Ilmir Bakirov <ilmirbakirov@gmail.com>
2016-07-23 01:20:32 +0500

    jpa rest

---
+++ Accessing-data-jpa-rest/src/main/java/com/bakigoal/Person.java
@@ -0,0 +1,36 @@
+package com.bakigoal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by ilmir on 23.07.16.
+ */
+@Entity
+public class Person {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+
+  private String firstName;
+  private String lastName;
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+}
